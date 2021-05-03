7a491e475644ba26b5bdd9f41ab028a8b8c38eb6
Matthew Rawlings <mattcanemail@gmail.com>
2021-05-03 00:25:03 -0700

    removing stacks from top level app

--- bin/serverless_boi.ts
+++ bin/serverless_boi.ts
@@ -1,15 +1,6 @@
 #!/usr/bin/env node
 import { App } from "monocdk";
-import { ApiGatewayStack } from "../lib/stacks/ApiGatewayStack";
-import { ServerlessBoiResourcesStack } from "../lib/stacks/ServerlessBoiResourcesStack";
 import { PipelineStack } from "../lib/stacks/PipelineStack";
 
 const app = new App();
 const pipeline = new PipelineStack(app, "ServerlessBoi-Pipeline");
-const resourcesStack = new ServerlessBoiResourcesStack(
-  app,
-  "ServerlessBoi-Resources-Stack"
-);
-new ApiGatewayStack(app, "ServerlessBoi-Api-Gateway-Stack", {
-  resourcesStack: resourcesStack,
-});
