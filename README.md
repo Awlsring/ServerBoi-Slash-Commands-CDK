f1393a51bd54936d348870ec58acd4ed71b40394
Matthew Rawlings <mattcanemail@gmail.com>
2021-06-09 20:17:26 -0700

    Converting from codepipeline to github actions

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
+![Build status](https://github.com/Awlsring/ServerBoi-Slash-Commands-CDK/workflows/Deploy/badge.svg "GitHub Actions Build Status")
+
 ## ServerlessBoi
--- bin/serverless_boi.ts
+++ bin/serverless_boi.ts
@@ -1,6 +1,17 @@
 #!/usr/bin/env node
 import { App } from "monocdk";
-import { PipelineStack } from "../lib/stacks/PipelineStack";
+import { ApiGatewayStack } from "../lib/stacks/ApiGatewayStack";
+import { ServerlessBoiResourcesStack } from "../lib/stacks/ServerlessBoiResourcesStack";
+import { ServerWorkflowsStack } from "../lib/stacks/ServerWorkflowsStack";
 
 const app = new App();
-const pipeline = new PipelineStack(app, "ServerlessBoi-Pipeline");
+
+const resourcesStack = new ServerlessBoiResourcesStack(app, "Resources-Stack");
+
+new ApiGatewayStack(app, "Api-Gateway-Stack", {
+  resourcesStack: resourcesStack,
+});
+
+new ServerWorkflowsStack(app, "Workflows-Stack", {
+  resourcesStack: resourcesStack,
+});
