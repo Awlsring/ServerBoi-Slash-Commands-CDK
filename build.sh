c574a5f48d2e695511c014ed4de79869e4fa5240
Matthew Rawlings <mattcanemail@gmail.com>
2021-06-12 19:59:59 -0700

    Packaged layers incorrectly

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ mkdir -p lambdas/layers/serverboi_utils/python/lib/python3.8/site-packages
 mkdir -p lambdas/layers/cloud_apis/python/lib/python3.8/site-packages
 
 pip install git+https://github.com/ServerBoiTeam/ServerBoi-Python-Utils.git --upgrade -t lambdas/layers/serverboi_utils/python/lib/python3.8/site-packages
-pip install linode_api4 --upgrade -t lambdas/layers/serverboi_utils/python/lib/python3.8/site-packages
+pip install linode_api4 --upgrade -t lambdas/layers/cloud_apis/python/lib/python3.8/site-packages
 cd lambdas/layers/serverboi_utils
 zip -r serverboi_utils.zip .
 rm -rf python
